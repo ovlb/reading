4e57e602c4db516f67bdb46a32e5058e3e461385
Oscar <o@ovlb.net>
2017-11-29 13:02:45 +0100

    add category pages

---
+++ pages/c/_category.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+  section.articles
+    post(v-for="post in filteredPosts" :post="post" :key="post.fields.id")
+</template>
+
+<script>
+import Post from '~/components/Post'
+
+export default {
+  components: {
+    Post
+  },
+  computed: {
+    filteredPosts () {
+      return this.$store.state.posts.filter((post) => {
+        const categories = post.fields.categories
+        let found = false
+
+        categories.forEach((category) => {
+          if (!found) {
+            if (category.fields.slug === this.$route.params.category) found = true
+          }
+        })
+
+        return found
+      })
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
---
+++ pages/c/index.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  section.articles
+    h1 Bitte wähle eine Kategorie aus.
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
