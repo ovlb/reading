58eca74c0f610112905a8897b6466be169171e53
Oscar <o@ovlb.net>
2017-12-06 21:46:51 +0100

    add meta page

---
+++ pages/meta/index.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+section.articles
+  h1.main-headline Überschrift
+  p reading.ovlb ist ein Experiment, eine Spielwiese, mit dem zusätzlichen Nutzen, dass ich ein paar der Artikel, die ich gerne gelesen habe, vielleicht nicht direkt wieder vergesse.
+  p Diese Seite nutzt <a href="https://vuejs.org/" title="Vue – The Progressive JavaScript Framework">Vue</a> und <a href="https://nuxtjs.org/" title="Nuxt – Universal Vue Applications">Nuxt</a>. Inhalte stelle ich bei <a href="https://www.contentful.com/" title="Contentful – Content Infrastructure for Digital Teams">Contentful</a> ein.
+  p Als Open-Graph-Image (das ist das, was du zum Beispiel im Share-Dialog von Facebook siehst) verwende ich <a href="https://unsplash.com/photos/Oaqk7qqNh_c" title="FIKA Cafe, Toronto, Canada von Patrick Tomasso">ein Bild</a> von <a href="https://unsplash.com/@impatrickt" title="Patrick Tomassos Profil bei Unsplash">Patrick Tomasso</a>.
+  p Die verwendeten Schriften sind <a href="https://www.myfonts.com/fonts/borutta/adagio-sans/" title="Adagio Sans bei MyFonts">Adagio_Sans</a> und <a href="https://www.myfonts.com/fonts/borutta/adagio-serif/" title="Adagio Serif bei MyFonts">Adagio_Serif</a>. Jeweils in den Schnitten Regular und Semibold. Adagio ist eine Schriftsippe der <a href="https://www.myfonts.com/foundry/Borutta/" title="Borutta be MyFonts">Borutta Type Foundry</a>.
+  p Code und Design von mir, <a href="https://www.ovlb.net/">Oscar</a>. Mit Hilfe des Internets. Der Code kann <a href="https://github.com/ovlb/reading" title="GitHub Repository dieser Seite">auf GitHub</a> eingesehen werden. Wenn du einen Bug entdeckst oder einen Verbesserungsvorschlag hast, eröffne dort ein <a href="https://github.com/ovlb/reading/issues" title="Issue-Tracker für reading.ovlb">Issue</a>, oder <a href="mailto:o@ovlb.net">schreib mir eine Mail</a>.
+  h2.sub-headline Kann ich ja nichts für
+  p Die verlinkten Artikel sind interessant, ihre Inhalte mache ich mir trotzdem nicht zu Eigen. Insbesondere, wenn diese sich ändern, nachdem ich sie gelesen habe. Auch mit Vue, Nuxt oder Contentful habe ich nichts zu tun.
+  h2.sub-headline Kannst du haben
+  p Diese Seite ist <a href="https://opensource.org/licenses/MIT" title="MIT License – Open Source Initiative">MIT lizensiert</a>. Du kannst mit ihr also machen, was du willst. Ich übernehme aber auch kein kleines bisschen Haftung, wenn du das machst, und irgendwas passiert. Bitte beachte, dass diese Lizenz für Teile der Seite – vor allem für die verwendeten Schriften und Auszüge der verlinkten Artikel – nicht automatisch gilt.
+  h2.sub-headline Impressum
+  p Oscar Braunert<br>Stralauer Allee 23a<br>10245 Berlin<br>o@ovlb.net
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
