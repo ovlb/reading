377f99047fb4d4a7ec0e3b46b77af1787d2f859f
Oscar <o@ovlb.net>
2018-01-20 01:41:55 +0100

    use dark netlify icon

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 .main-footer
   <a href="https://www.contentful.com/" rel="nofollow" target=“_blank”><img src="https://images.contentful.com/fo9twyrwpveg/44baP9Gtm8qE2Umm8CQwQk/c43325463d1cb5db2ef97fca0788ea55/PoweredByContentful_LightBackground.svg" class="hosting-icon" alt="Powered by Contentful" /></a>
-  <a href="https://www.netlify.com/" rel="nofollow" target=“_blank”><img src="https://www.netlify.com/img/global/badges/netlify-color-bg.svg" class="hosting-icon" alt="Deploys by Netlify" /></a>
+  <a href="https://www.netlify.com/" rel="nofollow" target=“_blank”><img src="https://www.netlify.com/img/global/badges/netlify-dark.svg" class="hosting-icon" alt="Deploys by Netlify" /></a>
 </template>
 
 <script>
