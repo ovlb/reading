62d15342e807a65e0100f26e08ee6db9792cfeae
Oscar <o@ovlb.net>
2018-01-21 17:50:57 +0100

    add loadMoreButton after list of posts

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,28 @@
 <template lang="pug">
   main.articles
     post(v-for="post in posts" :post="post" :key="post.sys.id")
+    load-more-button(v-if="totalIsLargerThanExisting")
 </template>
 
 <script>
 import Post from '~/components/Post'
+import LoadMoreButton from '~/components/LoadMoreButton'
 
 export default {
   components: {
-    Post
+    Post,
+    LoadMoreButton
     // CategoryList
   },
   data () {
     return {
-      nosts: 0
+      posts: this.$store.state.posts,
+      postTotal: this.$store.state.postTotal
     }
   },
   computed: {
-    posts () {
-      return this.$store.state.posts
+    totalIsLargerThanExisting () {
+      return this.postTotal > this.posts.length
     }
   }
 }
